667c8d1002c88e12870e100b03ff9025ff0c511d
Tyler Waters <tyler.waters@gmail.com>
2017-10-06 21:50:53 -0700

    Unit tests

---
+++ test/unit/ensure-config.test.js
@@ -0,0 +1,121 @@
+
+const path = require('path')
+const fs = require('fs')
+const assert = require('assert')
+
+const sinon = require('sinon')
+const proxyquire = require('proxyquire')
+
+let ensureConfig = null
+
+describe('ensure-config', () => {
+
+  let accessStub = null
+  let resolveStub = null
+
+  beforeEach(() => {
+    sinon.stub(process, 'cwd').returns('here')
+    accessStub = sinon.stub(fs, 'access').callThrough().withArgs('here').callsArg(1)
+    resolveStub = sinon.stub(path, 'resolve').callThrough().withArgs('here').returnsArg(0)
+    ensureConfig = proxyquire('../../lib/ensure-config', {path: {resolve: resolveStub}, fs: {access: accessStub}})
+  })
+
+  afterEach(() => {
+    process.cwd.restore()
+    fs.access.restore()
+    path.resolve.restore()
+  })
+
+  describe('good data', () => {
+
+    it('should use sane defaults', async () => {
+
+      const config = {}
+
+      await ensureConfig(config)
+
+      assert.deepEqual(config, {
+        name: 'here',
+        path: 'here',
+        remote: 'origin',
+        upstream: 'master',
+        links: []
+      })
+
+    })
+
+    it('should do nothing with everything provided', async () => {
+
+      const config = {
+        repositories: [{
+          name: 'some-name',
+          path: 'here',
+          upstream: 'some-upstream',
+          remote: 'some-remote',
+          links: ['some-module1', 'some-module2']
+        }, {
+          name: 'some-other-name',
+          path: 'here',
+          upstream: 'some-other-upstream',
+          remote: 'some-other-remote',
+          links: ['some-module1', 'some-module2']
+        }]
+      }
+
+      await ensureConfig(config)
+
+      assert.deepEqual({
+        repositories: [{
+          name: 'some-name',
+          path: 'here',
+          upstream: 'some-upstream',
+          remote: 'some-remote',
+          links: ['some-module1', 'some-module2']
+        }, {
+          name: 'some-other-name',
+          path: 'here',
+          upstream: 'some-other-upstream',
+          remote: 'some-other-remote',
+          links: ['some-module1', 'some-module2']
+        }]
+      }, config)
+
+    })
+
+  })
+
+  describe('bad data', () => {
+
+    it('should fail with bad paths', async () => {
+      accessStub.callsArgWith(1, new Error())
+      try { await ensureConfig({path: 'here'}); assert.ok(false) }
+      catch (err) { assert.equal(err.message, 'config is malformed, can\'t access here')}
+    })
+
+    it('should fail if repositories not an array', async () => {
+      try { await ensureConfig({repositories: 'nope'}); assert.ok(false) }
+      catch (err) { assert.equal(err.message, 'config is malformed, expected repositories to be array') }
+    })
+
+    it('should fail if upstream not string', async () => {
+      try { await ensureConfig({repositories: [{upstream: {}}]}); assert.ok(false) }
+      catch (err) { assert.equal(err.message, 'config is malformed, expected repository[0].upstream to be string') }
+    })
+
+    it('should fail if upstream not string', async () => {
+      try { await ensureConfig({repositories: [{remote: {}}]}); assert.ok(false) }
+      catch (err) { assert.equal(err.message, 'config is malformed, expected repository[0].remote to be string') }
+    })
+
+    it('should fail if links not an array', async () => {
+      try { await ensureConfig({links: 'nope'}); assert.ok(false) }
+      catch (err) { assert.equal(err.message, 'config is malformed, expected links to be array') }
+    })
+
+    it('should fail if one of the links is not a string', async () => {
+      try { await ensureConfig({links: [{}]}); assert.ok(false) }
+      catch (err) { assert.equal(err.message, 'config is malformed, expected links[0] to be string') }
+    })
+  })
+
+})
---
+++ test/unit/pull-install.test.js
@@ -0,0 +1,156 @@
+
+const path = require('path')
+const fs = require('fs')
+const assert = require('assert')
+const child_process = require('child_process')
+const pino = require('pino')
+
+const logger = require('../../lib/logger')
+const sinon = require('sinon')
+const proxyquire = require('proxyquire')
+
+let pullInstall = null
+
+describe('pull-install', () => {
+
+  let rimrafStub = null
+  let execStub = null
+  let config = null
+  const loggerStub = pino({level: 'silent'})
+  beforeEach(() => {
+    rimrafStub = sinon.stub().callsArg(1)
+    execStub = sinon.stub(child_process, 'exec').callsArgWith(2, null, {stdout: ''})
+    pullInstall = proxyquire('../../lib/pull-install', {
+      child_process: {exec: execStub},
+      rimraf: rimrafStub
+    })(loggerStub)
+    config = {
+      name: 'dummy',
+      upstream: 'master',
+      remote: 'origin',
+      path: 'here',
+      links: ['module-1', 'module-2']
+    }
+  })
+
+  afterEach(() => {
+    child_process.exec.restore()
+  })
+
+  it('with local branch missing, checks out and exists without packageJson changes', async () => {
+
+    execStub.onCall(1).callsArgWith(2, null, {stdout: 'some-rando-branch'}) // current branch
+    execStub.onCall(2).callsArgWith(2, null, {stdout: ''}) // can't find master
+    execStub.onCall(3).callsArgWith(2, null, {stdout: ''}) // no changes to packageJson
+
+    await pullInstall(config)
+
+    assert.equal(execStub.args[0][0], 'git fetch')
+    assert.equal(execStub.args[1][0], 'git rev-parse --abbrev-ref HEAD')
+    assert.equal(execStub.args[2][0], 'git branch --list master')
+    assert.equal(execStub.args[3][0], 'git rev-list origin/master...some-rando-branch -- package.json')
+    assert.equal(execStub.args[4][0], 'git checkout -b master origin/master')
+    assert.equal(rimrafStub.callCount, 0)
+
+  })
+
+  it('with local branch missing, checks out and installs with packageJson changes', async () => {
+
+    execStub.onCall(1).callsArgWith(2, null, {stdout: 'some-rando-branch'}) // current branch
+    execStub.onCall(2).callsArgWith(2, null, {stdout: ''}) // can't find master
+    execStub.onCall(3).callsArgWith(2, null, {stdout: 'some-commit!'}) // package-json changes found
+
+    await pullInstall(config)
+
+    assert.equal(execStub.args[0][0], 'git fetch')
+    assert.equal(execStub.args[1][0], 'git rev-parse --abbrev-ref HEAD')
+    assert.equal(execStub.args[2][0], 'git branch --list master')
+    assert.equal(execStub.args[3][0], 'git rev-list origin/master...some-rando-branch -- package.json')
+    assert.equal(execStub.args[4][0], 'git checkout -b master origin/master')
+    assert.equal(rimrafStub.args[0][0], path.join('here', 'node_modules'))
+    assert.equal(execStub.args[5][0], 'npm install')
+    assert.equal(execStub.args[6][0], 'npm link module-1')
+    assert.equal(execStub.args[7][0], 'npm link module-2')
+
+  })
+
+  it('with branch checked out and up-to-date, exits out', async () => {
+
+    execStub.onCall(1).callsArgWith(2, null, {stdout: 'master'}) // current branch
+    execStub.onCall(2).callsArgWith(2, null, {stdout: 'master'}) // master found
+    execStub.onCall(3).callsArgWith(2, null, {stdout: ''}) // changes to tree not found
+    execStub.onCall(4).callsArgWith(2, null, {stdout: ''}) // changes to package not found
+
+    await pullInstall(config)
+
+    assert.equal(execStub.args[0][0], 'git fetch')
+    assert.equal(execStub.args[1][0], 'git rev-parse --abbrev-ref HEAD')
+    assert.equal(execStub.args[2][0], 'git branch --list master')
+    assert.equal(execStub.args[3][0], 'git rev-list origin/master...master')
+    assert.equal(execStub.args[4][0], 'git rev-list origin/master...master -- package.json')
+    assert.equal(rimrafStub.callCount, 0)
+
+  })
+
+  it('with branch checked out, not up-to-date but no package.json changes', async () => {
+    execStub.onCall(1).callsArgWith(2, null, {stdout: 'master'}) // current branch
+    execStub.onCall(2).callsArgWith(2, null, {stdout: 'master'}) // master found
+    execStub.onCall(3).callsArgWith(2, null, {stdout: 'some-commit!'}) // changes to tree not found
+    execStub.onCall(4).callsArgWith(2, null, {stdout: ''}) // changes to package not found
+
+    await pullInstall(config)
+
+    assert.equal(execStub.args[0][0], 'git fetch')
+    assert.equal(execStub.args[1][0], 'git rev-parse --abbrev-ref HEAD')
+    assert.equal(execStub.args[2][0], 'git branch --list master')
+    assert.equal(execStub.args[3][0], 'git rev-list origin/master...master')
+    assert.equal(execStub.args[4][0], 'git rev-list origin/master...master -- package.json')
+    assert.equal(execStub.args[5][0], 'git pull --ff-only')
+    assert.equal(rimrafStub.callCount, 0)
+
+  })
+
+  it('with branch checked out, not up-to-date and package.json changes', async () => {
+    execStub.onCall(1).callsArgWith(2, null, {stdout: 'master'}) // current branch
+    execStub.onCall(2).callsArgWith(2, null, {stdout: 'master'}) // master found
+    execStub.onCall(3).callsArgWith(2, null, {stdout: 'some-commit!'}) // changes to tree not found
+    execStub.onCall(4).callsArgWith(2, null, {stdout: 'some-commit!'}) // changes to package not found
+
+    await pullInstall(config)
+
+    assert.equal(execStub.args[0][0], 'git fetch')
+    assert.equal(execStub.args[1][0], 'git rev-parse --abbrev-ref HEAD')
+    assert.equal(execStub.args[2][0], 'git branch --list master')
+    assert.equal(execStub.args[3][0], 'git rev-list origin/master...master')
+    assert.equal(execStub.args[4][0], 'git rev-list origin/master...master -- package.json')
+    assert.equal(execStub.args[5][0], 'git pull --ff-only')
+    assert.equal(rimrafStub.args[0][0], path.join('here', 'node_modules'))
+    assert.equal(execStub.args[6][0], 'npm install')
+    assert.equal(execStub.args[7][0], 'npm link module-1')
+    assert.equal(execStub.args[8][0], 'npm link module-2')
+
+  })
+
+  it('with branch not checked out, not up-to-date and package.json changes', async () => {
+    execStub.onCall(1).callsArgWith(2, null, {stdout: 'some-rando-branch'}) // current branch
+    execStub.onCall(2).callsArgWith(2, null, {stdout: 'master'}) // master found
+    execStub.onCall(3).callsArgWith(2, null, {stdout: 'some-commit!'}) // changes to tree not found
+    execStub.onCall(4).callsArgWith(2, null, {stdout: 'some-commit!'}) // changes to package not found
+
+    await pullInstall(config)
+
+    assert.equal(execStub.args[0][0], 'git fetch')
+    assert.equal(execStub.args[1][0], 'git rev-parse --abbrev-ref HEAD')
+    assert.equal(execStub.args[2][0], 'git branch --list master')
+    assert.equal(execStub.args[3][0], 'git rev-list origin/master...master')
+    assert.equal(execStub.args[4][0], 'git rev-list origin/master...master -- package.json')
+    assert.equal(execStub.args[5][0], 'git checkout master')
+    assert.equal(execStub.args[6][0], 'git pull --ff-only')
+    assert.equal(rimrafStub.args[0][0], path.join('here', 'node_modules'))
+    assert.equal(execStub.args[7][0], 'npm install')
+    assert.equal(execStub.args[8][0], 'npm link module-1')
+    assert.equal(execStub.args[9][0], 'npm link module-2')
+
+  })
+
+})
